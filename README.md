756a07eb265d614ce8c05ce2b117de6fb94ac5d6
kangprog <thtjth123@naver.com>
2022-03-21 23:15:14 +0900

    feat: v3.2.0 + Metastore HA 추가   + master-slave replication + Hive 1개 추가 + tool.py 수정 + README.md 수정

--- README.md
+++ README.md
@@ -1,30 +1,67 @@
-## 실행 방법
+# 실행 방법
 
-#### HDFS Cluster
-```python
-# python tool.py build
-# python tool.py up
-# python tool.py down
+1. 도커 이미지 빌드
+```text
+> python tool.py hdfs build
+> python tool.py hive build
+> python tool.py metastore build
+```
+---
+2. HDFS 실행
+- 실행
+```text
+> python tool.py hdfs up
+```
+- 동작 확인 (Yarn HA 경우 Standby RM은 webUI 접속 안됨)
+```text
+NN HA: http://127.0.0.1:50070 or 50080
+RM HA: http://127.0.0.1:8088 or 8089
+```
+---
+3. Metastore 실행
+- 실행
+```text
+> python tool.py metastore up
 ```
 
-#### HS2, metastore, beeline
-- base image build
-```python
-# python tool.py hive build
+- replication 설정 (master-slave)
+
+**ref url:** https://jupiny.com/2017/11/07/docker-mysql-replicaiton/
+
+**master 설정**
+```text
+> docker exec -ti metastore-master mysql -uroot -p
+Password: mysql
+
+mysql> CREATE USER 'repluser'@'%' IDENTIFIED BY 'replpw'; 
+mysql> GRANT REPLICATION SLAVE ON *.* TO 'repluser'@'%';
+mysql> show master status;  
+ex.
++------------------+----------+--------------+------------------+-------------------+
+| File             | Position | Binlog_Do_DB | Binlog_Ignore_DB | Executed_Gtid_Set |
++------------------+----------+--------------+------------------+-------------------+
+| mysql-bin.000003 |      603 |              |                  |                   |
++------------------+----------+--------------+------------------+-------------------+
+
+이후, 아래 2개의 정보는 slave에서 replication Master 설정 시, 사용해야 함.
+File: mysql-bin.000003
+Position: 603
 ```
 
-- metastore Up, Down & Setting
-```python
-# metastore up & down
+**slave 설정**
+```text
+> docker exec -ti metastore-slave mysql -uroot -p
+Password: mysql
 
-# python tool.py hive metastore up
-# python tool.py hive metastore down
+mysql> CHANGE MASTER TO MASTER_HOST='metastore-master', MASTER_USER='repluser', MASTER_PASSWORD='replpw', MASTER_LOG_FILE='mysql-bin.000003', MASTER_LOG_POS=603;
+mysql> start slave;  
+mysql> show slave status;
 ```
-```python
-# metastore setting
 
-# docker exec -ti metastore mysql -uroot -p
-password: mysql
+- hive metastore 설정
+```text
+> docker exec -ti metastore-master mysql -uroot -p
+Password: mysql
 
 mysql> create database metastore default character set utf8;
 mysql> create user 'hive'@'%' identified by '123456';
@@ -32,46 +69,18 @@ mysql> grant all privileges on metastore.* to 'hive'@'%';
 mysql> flush privileges;
 mysql> exit;
 ```
-
-- hiveserver Up & Down
-```python
-# python tool.py hive up
-# python tool.py hive down
-# docker exec -ti hs01 bash
-# passwd
-123456
-```
-
-- beeline test client Up & Down
-```python
-# python tool.py hive beeline up
-# python tool.py hive beeline down
-```
-```python
-# connect hive server use beeline.
-
-# docker exec -ti beeline bash
-# beeline
-beeline>!connect jdbc:hive2://hs01:10000 root 123456
-
-0: jdbc:hive2://hs01:10000> set hive.execution.engine=tez;
-0: jdbc:hive2://hs01:10000> 
-
-```
-
 ---
-
-## 구동 확인
-```python
-# docker ps -a
-# http://127.0.0.1:50070 접속 후, active NN 확인 
-# http://127.0.0.1:50080 접속 후, standby NN 확인
+4. Hive 실행
+```text
+> python tool.py hive up
 ```
 
-```python
-# hive(TEZ)
+- HiveQL 접속 방법 및 테스트
+```text
+> docker exec -ti hs01 hive
+or
+> docker exec -ti hs02 hive
 
-# docker exec -ti hs01 hive
 hive> set hive.execution.engine=tez;
 hive> create database test;
 hive> use test;
@@ -82,40 +91,18 @@ hive> create table test.tab1(
 hive> show tables;
 hive> insert into table test.tab1
     > select 1 as col1, 'test' as col2;
-
-# http://127.0.0.1:8088 들어가서 쿼리 동작, 엔진 확인
+```
+ps. 만약 return code 1 error 발생 시, 메모리 자원할당 부족 에러임.
+**필요 메모리 량에 맞게 설정**
+```
+ex.
+hive> set tez.am.resource.memory.mb=3072;
 ```
 
----
-
-## ISSUE
-- zkServer.sh가 실행이 안되는 문제... 3888에 connection refuse 뜬다.
-  - 해결방법:
-  https://stackoverflow.com/questions/30940981/zookeeper-error-cannot-open-channel-to-x-at-election-address
-  - ㅈㄴ 어이없음.. zknode 3개에서 빠르게 동시에 실행시키면 됨.....
-  
-  ```commandline
-  /opt/zookeeper/bin/zkServer.sh start
-  /opt/zookeeper/bin/zkServer.sh status
-  ```
-  
-- hive tez engine 설정 후, insert into 시, virtual Memeory 부족으로 인하여 task return 1 에러 발생
-  - 해결방법: https://118k.tistory.com/568
-  - 우선 hive 실행을 Debug Level로 실행 후 자세한 내용 파악 필요.
-  - 경험한 에러는 2.4G를 사용하는데 2.1G 할당되서 발생하는 오류였음. 3G 할당 후 성공 확인
-  ```text
-  # 에러 상세 내용 중 일부
-  Container [pid=352,containerID=container_1647588320530_0001_02_000001] is running beyond virtual memory limits. Current usage: 165.8 MB of 1 GB physical memory used; 2.4 GB of 2.1 GB virtual memory used. Killing container.
-  ```
-  ```python
-  > hive --hiveconf hive.root.logleve=DEBUG,console
-  > set tez.am.resource.memory.mb=3072;
-  ```
-- 위 에러 해결 후, session timeout 에러 발생
-  - 해결방법: https://blog.csdn.net/liuxiao723846/article/details/70674803
-  - $TEZ_HOME/lib/commons-collections4-4.1.jar을 $HADOOP_HOME/lib으로 복사해주자
----
-
-## 참고
-https://devfon.tistory.com/43
-https://earthconquest.tistory.com/233
+- beeline을 통한 접속 방법
+```text
+> docker exec -ti beeline bash
+> beeline -u  "jdbc:hive2://hs01:10000?hive.execution.engine=tez"
+or
+> beeline -u  "jdbc:hive2://hs02:10001?hive.execution.engine=tez"
+```
--- tool.py
+++ tool.py
@@ -1,66 +1,64 @@
 import os
 import sys
 
-if sys.argv[1] == "build":
-    os.system("docker build -t hadoop_base:latest .")
-    os.system("docker image prune")
-    print("Base Image Build")
+if sys.argv[1] == "hdfs":
+    if sys.argv[2] == "build":
+        os.system("docker build -t hadoop_base:latest .")
+        os.system("docker image prune")
 
-elif sys.argv[1] == "up":
-    os.system("docker-compose -f docker-compose.yml up -d nn01")
-    os.system("docker-compose -f docker-compose.yml up -d nn02")
-    os.system("docker-compose -f docker-compose.yml up -d dn01")
-    os.system("docker-compose -f docker-compose.yml up -d dn02")
-    os.system("docker-compose -f docker-compose.yml up -d dn03")
-    print("Compose up")
+    if sys.argv[2] == "up":
+        os.system("docker-compose -f docker-compose.yml up -d nn01")
+        os.system("docker-compose -f docker-compose.yml up -d nn02")
+        os.system("docker-compose -f docker-compose.yml up -d dn01")
+        os.system("docker-compose -f docker-compose.yml up -d dn02")
+        os.system("docker-compose -f docker-compose.yml up -d dn03")
 
-elif sys.argv[1] == "down":
-    os.system("docker-compose -f docker-compose.yml stop nn01")
-    os.system("docker-compose -f docker-compose.yml stop nn02")
-    os.system("docker-compose -f docker-compose.yml stop dn01")
-    os.system("docker-compose -f docker-compose.yml stop dn02")
-    os.system("docker-compose -f docker-compose.yml stop dn03")
+    if sys.argv[2] == "down":
+        os.system("docker-compose -f docker-compose.yml stop nn01")
+        os.system("docker-compose -f docker-compose.yml stop nn02")
+        os.system("docker-compose -f docker-compose.yml stop dn01")
+        os.system("docker-compose -f docker-compose.yml stop dn02")
+        os.system("docker-compose -f docker-compose.yml stop dn03")
 
-    os.system("docker-compose -f docker-compose.yml rm nn01")
-    os.system("docker-compose -f docker-compose.yml rm nn02")
-    os.system("docker-compose -f docker-compose.yml rm dn01")
-    os.system("docker-compose -f docker-compose.yml rm dn02")
-    os.system("docker-compose -f docker-compose.yml rm dn03")
-    print("Compose Down & Volume all remove")
+        os.system("docker-compose -f docker-compose.yml rm nn01")
+        os.system("docker-compose -f docker-compose.yml rm nn02")
+        os.system("docker-compose -f docker-compose.yml rm dn01")
+        os.system("docker-compose -f docker-compose.yml rm dn02")
+        os.system("docker-compose -f docker-compose.yml rm dn03")
 
-elif sys.argv[1] == "hive":
+if sys.argv[1] == "metastore":
+    if sys.argv[2] == "build":
+        os.system("cd ./mysql/master && docker build -t metastore-master:latest .")
+        os.system("cd ./mysql/slave && docker build -t metastore-slave:latest .")
+        os.system("docker image prune")
+
+    if sys.argv[2] == "up":
+        os.system("docker-compose -f docker-compose.yml up -d metastore-master")
+        os.system("docker-compose -f docker-compose.yml up -d metastore-slave")
+
+    if sys.argv[2] == "down":
+        os.system("docker-compose -f docker-compose.yml stop metastore-master")
+        os.system("docker-compose -f docker-compose.yml stop metastore-slave")
+
+        os.system("docker-compose -f docker-compose.yml rm metastore-master")
+        os.system("docker-compose -f docker-compose.yml rm metastore-slave")
+
+if sys.argv[1] == "hive":
     if sys.argv[2] == "build":
         os.system("cd ./hive && docker build -t hive_base:latest .")
         os.system("docker image prune")
-        print("HIVE Base Image Build")
 
-    elif sys.argv[2] == "metastore":
-        if sys.argv[3] == "up":
-            os.system("docker-compose -f docker-compose.yml up -d metastore")
-            print("HIVE Metastore Mysql up")
-        elif sys.argv[3] == "down":
-            os.system("docker-compose -f docker-compose.yml stop metastore")
-            os.system("docker-compose -f docker-compose.yml rm metastore")
-            print("HIVE Metastore Mysql down")
-
-    elif sys.argv[2] == "beeline":
-        if sys.argv[3] == "up":
-            os.system("docker-compose -f docker-compose.yml up -d beeline")
-            print("HIVE Metastore Mysql up")
-        elif sys.argv[3] == "down":
-            os.system("docker-compose -f docker-compose.yml stop beeline")
-            os.system("docker-compose -f docker-compose.yml rm beeline")
-            print("beeline client down")
-
-
-    elif sys.argv[2] == "up":
+    if sys.argv[2] == "up":
         os.system("docker-compose -f docker-compose.yml up -d hs01")
-        print("HIVE up")
-    elif sys.argv[2] == "down":
+        os.system("docker-compose -f docker-compose.yml up -d hs02")
+        os.system("docker-compose -f docker-compose.yml up -d beeline")
+
+    if sys.argv[2] == "down":
         os.system("docker-compose -f docker-compose.yml stop hs01")
+        os.system("docker-compose -f docker-compose.yml up -d hs02")
+        os.system("docker-compose -f docker-compose.yml stop beeline")
+
         os.system("docker-compose -f docker-compose.yml rm hs01")
-        print("HIVE down")
-
-
-
+        os.system("docker-compose -f docker-compose.yml up -d hs02")
+        os.system("docker-compose -f docker-compose.yml rm beeline")
 
